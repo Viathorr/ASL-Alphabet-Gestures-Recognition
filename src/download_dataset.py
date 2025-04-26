b5725211859da1d671cfc500fecef903519c8e76
Viathorr <142295754+Viathorr@users.noreply.github.com>
2025-04-26 20:32:36 +0300

    Fix directory check in dataset downloader and add data exploration notebook.

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -3,12 +3,12 @@ import kaggle
 
 root_dir = os.getcwd()
 
-if root_dir.endswith("scripts"):
-    # If the current working directory ends with "scripts", go up one level
+if root_dir.endswith("src"):
+    # If the current working directory ends with "src", go up one level
     root_dir = os.path.dirname(root_dir)
 
 data_dir = os.path.join(root_dir, "data")
-dataset_name = "lexset/synthetic-asl-alphabet"
+dataset_name = "lexset/synthetic-asl-alphabet"  # synthetic data
 unzip = True
 
 # Make sure to have your Kaggle API key saved in your home directory
@@ -20,4 +20,4 @@ os.makedirs(data_dir, exist_ok=True)
 print(f"Downloading dataset: `{dataset_name}` ...") 
 kaggle.api.dataset_download_files(dataset_name, path=data_dir, unzip=unzip)
 
-print("✅ Dataset downloaded successfully.")
+print(f"✅ Dataset {dataset_name} downloaded successfully.")
